4465ab6c20110c5818130b7c28556b75229b1cac
Tóth Péter <peeter.toth@gmail.com>
2016-07-28 14:51:40 +0200

    0.1 verzió, frissített README.md

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # ncore-torrent-szinezes
 feltöltési dátum mezőjének színezése
+
+A /torrents.php?miszerint* oldalon működik, tehát rendezni kell a torrenteket mondjuk feltöltők száma szerint, hogy működésbe lépjen a színezés.
+
+Színkód | Hetek száma |Szín
+--------|------------|---
+'#2ECC71'| 1 | Zöld árnyalatai
+'#58D68D'| 2
+'#82E0AA'| 3
+'#ABEBC6'| 4
+'#D5F5E3'| 5
+'#3498DB'| 6 | Kék árnyalatai
+'#5DADE2'| 7
+'#85C1E9'| 8
+'#AED6F1'| 9
+'#D6EAF8'| 10
+'#8E44AD'| 11 | Lila árnyalatai
+'#A569BD'| 12
+'#BB8FCE'| 13
+'#D2B4DE'| 14
+'#E8DAEF'| 15
---
+++ ncore-torrent-szinezes.user.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         ncore-torrents-szinezes
+// @namespace    peetftp.ddns.net
+// @version      0.1
+// @description  feltöltés ideje alapján szinezi a boxok hátterét
+// @author       kyle
+// @match        https://ncore.cc/torrents.php?miszerint*
+// @grant        none
+// @require      https://code.jquery.com/ui/1.11.4/jquery-ui.js
+// @updateURL    https://raw.githubusercontent.com/peetertoth/ncore-torrent-szinezes/master/ncore-torrent-szinezes.user.js
+// @downloadURL  https://raw.githubusercontent.com/peetertoth/ncore-torrent-szinezes/master/ncore-torrent-szinezes.user.js
+// ==/UserScript==
+
+$(document).ready(function() {
+    l = document.getElementsByClassName('box_feltoltve2');
+    colors = ['#2ECC71', '#58D68D', '#82E0AA', '#ABEBC6', '#D5F5E3',
+              '#3498DB', '#5DADE2', '#85C1E9', '#AED6F1', '#D6EAF8',
+              '#8E44AD', '#A569BD', '#BB8FCE', '#D2B4DE', '#E8DAEF'];
+    cDate = new Date();
+
+    for (i=0; i < l.length; ++i) {
+        sDate = l[i].innerHTML.split('<br>')[0];
+        date = new Date(sDate);
+
+        for (j=0; j < colors.length; ++j) {
+            compareDate = new Date();
+            compareDate.setDate(compareDate.getDate() - (j+1)*7);
+
+            if (date > compareDate) {
+                l[i].style.backgroundColor = colors[j];
+                break;
+            }
+        }
+    }
+});
